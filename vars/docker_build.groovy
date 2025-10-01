867f5833c35a03525aa48e1fee4ea93d84b6ad53
Amish88-eng <alamamish88@gmail.com>
2025-10-01 10:18:44 +0000

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 def call(String Project_name, String imageTag, String Dockerhub){
   echo "Building Phase"
-  sh "docker build -t "${Dockerhub}"/"${Project_name}":"${imageTag}" . "
+  sh "docker build -t ${Dockerhub}/${Project_name}:${imageTag} . "
 }
